4b1b1e8089c33587c53986d43f97ada708fc54ae
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2024-07-28 21:57:56 -0300

    change layout change bg color

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="bg-sky-100 min-h-screen">{children}</div>
+      </body>
     </html>
   );
 }
