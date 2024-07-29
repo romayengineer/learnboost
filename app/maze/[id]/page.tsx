13ac363992f0efffd36ff6b53ed2034a029f758d
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2024-07-28 21:57:58 -0300

    redirect to 404 if maze not found

--- app/maze/[id]/page.tsx
+++ app/maze/[id]/page.tsx
@@ -1,10 +1,14 @@
 import { login, getFlashcards } from "@/app/db";
+import { redirect } from "next/navigation";
 import FlashCard from "@/components/flashcard";
 
 export default async function MazeID({ params }: { params: { id: string } }) {
   let mazeId = params.id;
   let db = await login();
   let flashcards = await getFlashcards(db, mazeId);
+  if (flashcards.length === 0) {
+    redirect("/not-found");
+  }
   return (
     <main>
       <h1>Maze {mazeId}</h1>
