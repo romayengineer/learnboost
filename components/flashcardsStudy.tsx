4b27d45b6ed7bfc213398fbbe5d76876e2085e5c
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2024-07-28 21:58:24 -0300

    new module dbUtils.ts

--- components/flashcardsStudy.tsx
+++ components/flashcardsStudy.tsx
@@ -5,34 +5,7 @@ import FlashcardsCounter from "./flashcardsCounter";
 import { getRecalls, login, sendRecall } from "@/app/db";
 import Client from "pocketbase";
 import { GroupedRecalls, RecallData, RecallsData } from "@/app/dbTypes";
-
-const getRandomIndex = (arrayData: Array<any>) => {
-  return Math.floor(arrayData.length * Math.random());
-};
-
-/**
- * Takes an array of flashcard recall data and consolidates it into a summary
- * object. It aggregates the total time spent and total "easy" score for each
- * unique flashcard ID. This function streamlines flashcard review data, making
- * it easier to analyze performance across multiple study sessions by providing
- * a concise overview of each flashcard's cumulative review statistics.
- *
- * @param recalls all the recalls from database
- * @returns a map of flashcardId to an object with the totalTime and the
- * TotalEasy
- */
-function groupRecallsByFlashcardId(recalls: RecallsData): GroupedRecalls {
-  var groupedRecalls: GroupedRecalls = {};
-  recalls.forEach((recall) => {
-    const prevVal = groupedRecalls[recall.flashcardId] || {};
-    groupedRecalls[recall.flashcardId] = {
-      totalTime: (prevVal.totalTime || 0) + recall.timeBack,
-      totalEasy: (prevVal.totalEasy || 0) + recall.easy,
-    };
-  });
-  console.log("DEBUG groupRecallsByFlashcardId: ", groupedRecalls);
-  return groupedRecalls;
-}
+import { getRandomIndex, groupRecallsByFlashcardId } from "@/app/dbUtils";
 
 export default function FlashcardsStudy(params: {
   flashcards: Array<{ id: string; front: string; back: string }>;
