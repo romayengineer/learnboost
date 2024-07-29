b871d8a171d486db8200e4943daefeac8ab0fc6d
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2024-07-28 21:58:03 -0300

    remove the study button if the maze is empty

--- components/mazeFlashcards.tsx
+++ components/mazeFlashcards.tsx
@@ -8,12 +8,14 @@ export default function MazeFlashcards(params: {
   const flashcards = params.flashcards;
   return (
     <div className="mt-10">
-      <a
-        className="text-xl ml-4 pl-6 pr-6 pt-2 pb-2 rounded-full bg-blue-300"
-        href={`/maze/${params.mazeId}/study`}
-      >
-        Study!
-      </a>
+      {flashcards.length > 0 && (
+        <a
+          className="text-xl ml-4 pl-6 pr-6 pt-2 pb-2 rounded-full bg-blue-300"
+          href={`/maze/${params.mazeId}/study`}
+        >
+          Study!
+        </a>
+      )}
       <div className="mt-6 grid gap-4 rid-cols-1 sm:grid-cols-2 md:grid-cols-3">
         {flashcards.map((flashcard) => {
           return (
