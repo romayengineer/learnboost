0538c5c3eb6485bd3189d5843f079b2113a63df4
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2024-07-28 21:57:52 -0300

    new flashcard hidden component for studying

--- components/flashcardHidden.tsx
+++ components/flashcardHidden.tsx
@@ -1,12 +1,36 @@
+"use client";
+import React from "react";
+
 export default function FlashCardHidden(params: {
   front: string;
   back: string;
 }) {
+  // TODO collapse CSS class makes the div dissapear
+  const [showBack, setShowBack] = React.useState(false);
+
+  var toggleShowBack = () => {
+    const newShowBack = !showBack;
+    setShowBack(newShowBack);
+  };
+
   return (
-    <div className="m-4 p-6 bg-stone-300 max-w-sm rounded overflow-hidden shadow-lg">
-      <div>{params.front}</div>
-      <br />
-      <div>{params.back}</div>
+    <div
+      style={{ width: "600px" }}
+      className="p-6 p-4 border-4 collapse-open border bg-stone-300"
+      onClick={toggleShowBack}
+    >
+      <input type="radio" name="my-accordion-1" hidden />
+      <div className="collapse-title text-xl font-medium">
+        <p>{params.front}</p>
+      </div>
+      {showBack && (
+        <div>
+          <br />
+          <div className="collapse-content">
+            <p>{params.back}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
