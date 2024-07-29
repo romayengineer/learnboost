26970ddba6b3ce7be676007cfb67007b1bd8f133
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2024-07-28 21:57:58 -0300

    new 404 not found page

---
+++ app/not-found.jsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+    return (
+        <div className="flex flex-row justify-center items-center">
+            <div>
+                <div style={{height: "200px"}}></div>
+                <h1 className="text-6xl pl-7">404</h1><br/>
+                <h2 className="text-4xl">Not Found</h2>
+            </div>
+        </div>
+    )
+}
