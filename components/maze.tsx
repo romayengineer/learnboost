bb52df0e86000d1eb7a278dd9c96e6eea37461f2
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2024-07-28 21:58:03 -0300

    the mazes have a height

--- components/maze.tsx
+++ components/maze.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 export default function Maze(params: { mazeId: string; mazeName: string }) {
   return (
     <Link href={`/maze/${params.mazeId}`}>
-      <div className="p-6 bg-sky-300 max-w-sm rounded overflow-hidden shadow-lg">
+      <div
+        style={{ height: "100px" }}
+        className="p-6 bg-sky-300 max-w-sm rounded overflow-hidden shadow-lg"
+      >
         {params.mazeName}
       </div>
     </Link>
