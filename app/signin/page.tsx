4b904274015a9297501ecba491659842f3d61a20
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2024-07-28 21:57:58 -0300

    new signin and signup forms

---
+++ app/signin/page.tsx
@@ -0,0 +1,33 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="min-h-screen flex flex-row justify-center">
+        <form action="javascript:void(0);">
+          <div style={{ height: "150px" }}></div>
+          <h1 className="text-2xl mb-4">Login</h1>
+          <input
+            style={{ width: "300px" }}
+            className="mb-6 p-2 rounded-full"
+            type="text"
+            name="username"
+            placeholder="Username..."
+          />
+          <br />
+          <input
+            style={{ width: "300px" }}
+            className="mb-6 p-2 rounded-full"
+            type="text"
+            name="password"
+            placeholder="Password..."
+          />
+          <br />
+          <div className="flex flex-row justify-center">
+            <button className="bg-sky-300 pl-6 pr-6 pt-2 pb-2 rounded-2xl">
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+}
--- components/navBar.tsx
+++ components/navBar.tsx
@@ -2,10 +2,7 @@ export default function NavBar() {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
             src="/learnboost-logo.png"
             className="h-8"
@@ -43,7 +40,7 @@ export default function NavBar() {
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <a
-                href="#"
+                href="/signin"
                 className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                 aria-current="page"
               >
@@ -52,7 +49,7 @@ export default function NavBar() {
             </li>
             <li>
               <a
-                href="#"
+                href="/signup"
                 className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                 aria-current="page"
               >
