45bc35debbc7062f33518bbf64d827fdd608be5a
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2024-07-28 21:57:49 -0300

    new maze list view

---
+++ app/db.tsx
@@ -0,0 +1,19 @@
+import PocketBase, { RecordModel } from "pocketbase";
+
+export async function login() {
+  const pb = new PocketBase(process.env.POCKETBASE_URL);
+
+  await pb.admins.authWithPassword(
+    process.env.DB_USER_NAME!,
+    process.env.DB_PASSWORD!,
+    {
+      cache: "no-store",
+    }
+  );
+
+  return pb;
+}
+
+export async function getMazes(pb: PocketBase): Promise<RecordModel[]> {
+  return pb.collection("mazes").getFullList();
+}
--- app/maze/page.tsx
+++ app/maze/page.tsx
@@ -1,7 +1,19 @@
-export default function Maze() {
+import { login, getMazes } from "../db";
+
+export default async function Maze() {
+  var pb = await login();
+  var mazes = await getMazes(pb);
   return (
-    <main>
+    <main className="p-6">
       <h1>Here are all your mazes!</h1>
+      <br />
+      {mazes.map((maze) => {
+        return (
+          <div className="p-6 bg-cyan-300 max-w-sm rounded overflow-hidden shadow-lg">
+            {maze.name}
+          </div>
+        );
+      })}
     </main>
   );
 }
